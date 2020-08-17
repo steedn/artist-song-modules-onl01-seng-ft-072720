78b4314a1462365476d8f45654e7ffb717b92d5b
Nathan Steed <steedn@outlook.com>
2020-08-17 05:09:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,19 +2,16 @@ require 'pry'
 require_relative '../lib/concerns/memorable'
 
 class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
-  extend Memorable
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
 
   def self.all
     @@songs
@@ -22,9 +19,6 @@ class Song
 
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+    artist.add_song(self) unless artist.songs.include?(self)
   end
 end
